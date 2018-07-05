c0a32df2066f219e2e33ed5b6ca65deb0eb3aed6
shehabam <shehab2014kuwait@gmail.com>
2018-07-05 18:53:44 +0300

    first push

--- restaurants/views.py
+++ restaurants/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Restaurant
+from .forms import RestaurantForm
 
 def welcome(request):
     return render(request, 'index.html', {'msg':'Hello World!'})
@@ -18,8 +19,15 @@ def restaurant_detail(request, restaurant_id):
     return render(request, 'detail.html', context)
 
 def restaurant_create(request):
+    restaurant = RestaurantForm()
+    if request.method == "POST":
+        restaurant = RestaurantForm(request.POST)
+        if restaurant.is_valid():
+            restaurant.save()
+            return redirect('restaurant-list')
 
     context = {
-
+        'restaurant': restaurant
     }
+
     return render(request, 'create.html', context)
